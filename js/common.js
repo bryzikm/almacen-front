4d0a92360892029fb6045ea3808cf4ed06d764f0
Mateusz <bryzikm@gmail.com>
2016-11-10 01:11:58 +0100

    [master] - adding dashboard

--- js/common.js
+++ js/common.js
@@ -9,12 +9,84 @@ $(document).ready(function () {
 
     $(window).on('scroll', function () {
 
-        if($(window).scrollTop() > 50 ) {
+        if ($(window).scrollTop() > 50) {
             $('#header').addClass('fixed');
         } else {
             $('#header').removeClass('fixed');
         }
 
+    });
+
+    $('#btn-login').on('click', function (e) {
+        e.preventDefault();
+
+        $('#modal-login').modal('show');
+    });
+
+    $('#btn-register').on('click', function (e) {
+        e.preventDefault();
+
+        $('#modal-register').modal('show');
+    });
+
+    $('#login-submit').on('click', function (e) {
+        e.preventDefault();
+
+        window.location.href = "dashboard.html";
+    });
+
+    $('#btn-logout').on('click', function (e) {
+        e.preventDefault();
+
+        window.location.href = "index.html";
+    });
+
+    $('.delete-folder').on('click', function () {
+        var temp = $(this);
+
+        temp.closest('.folder').remove();
+    });
+
+    $('.delete-file').on('click', function () {
+        var temp = $(this);
+
+        temp.closest('.file').remove();
+    });
+
+    $('#btn-edit').on('click', function (e) {
+        e.preventDefault();
+
+        $('#modal-edit').modal('show');
+    });
+
+    $('#btn-upload').on('click', function (e) {
+        e.preventDefault();
+
+        $('#modal-upload').modal('show');
+    });
+
+    $('#btn-create').on('click', function (e) {
+        e.preventDefault();
+
+        $('#modal-create').modal('show');
+    });
+
+    $('#mobile-button').on('click', function () {
+
+        if ($('#header .button__bar').is(':hidden')) {
+            $('#header .button__bar').show();
+
+            if(!$('#header').hasClass('fixed')) {
+                $('#header').addClass('fixed');
+            }
+        } else {
+            $('#header .button__bar').hide();
+        }
+
+    });
+
+    $(window).resize(function () {
+        $('#header .button__bar').hide();
     });
 
     /****************** SLOW SCROLLING ********************************************************************************/
